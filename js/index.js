c16857df7aa1f7a986d3c853a75f8d4d5287bdd8
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2018-12-10 09:17:40 +0200

    Initial commmit

---
+++ js/index.js
@@ -0,0 +1,4 @@
+var nameCourse = "Mate Academy";
+var namesOfGroup = ["de","dr","fr", "fe_oct18"];
+var ourGroupName = "fe_oct18";
+var ourStudentsNames = ["Yulia","Anton","Sergei","Nika","Roma","Ivan", "Alina", "Yura", "Vlad", "Dima"];
---
+++ js/mcdonalds.js
@@ -0,0 +1,43 @@
+function isCorrectAmount(n) {
+  var nNumber = parseFloat(n);
+  return !isNaN(nNumber) && (nNumber >= 0);
+}
+
+var amountOfMoney = prompt ("How much money do you have?","");
+
+while (!isCorrectAmount(amountOfMoney)) {
+  amountOfMoney = prompt ("Please, enter  again. How much money do you have","100");
+}
+
+function showResults(result , text1, text2) {
+  switch (result) {
+    case true :
+    console.log(text1);
+    break;
+    case false :
+    console.log(text2);
+    break;
+  }
+}
+
+function yourOrderIs(amountOfMoney) {
+  var result;
+  if (amountOfMoney > 100) {
+    result = confirm ("You can buy BigMacMenu. Wish?");
+    showResults(result, "Your order is BigMacMenu", "You do not order anything");
+  }  
+  else if (amountOfMoney >= 50 && amountOfMoney < 100) {
+    result = confirm ("You can buy hamburger and french fries. Wish?");
+    showResults(result, "Your order is hamburger and french fries", "You do not order anything");
+  } 
+  else if (amountOfMoney < 50 && amountOfMoney >= 20) {
+    result = confirm ("you can buy coca cola. Wish?");
+    showResults(result, "Your order is coca cola", "You do not order anything"); 
+  } 
+  else if (amountOfMoney <20) {
+    result = console.log("Sorry we do not have dishes for you here");
+    console.log("You do not order anything");
+  }  
+}
+
+yourOrderIs(amountOfMoney);
